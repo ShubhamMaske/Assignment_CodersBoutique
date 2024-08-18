acd115782aecfa5e22d37b12d9a4c38f2411c375
Shubham Maske <shubhammaske099@gmail.com>
2024-08-18 09:03:42 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import sequelize from './config/database.js'
+import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import 'dotenv/config'
+
+const app = express()
+const PORT = process.env.PORT || 3000
+dotenv.config();
+
+app.use(express.json())
+app.use('/api/auth',authRoutes)
+app.use('/api/user',userRoutes)
+
+sequelize.sync({ force: false })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server is listning on port ${PORT}`));
+  })
+  .catch(err => console.log('Error: ' + err));
